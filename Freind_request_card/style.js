57ef972b13c1086b8bf6379de416712a43ed2cd9
Samrat <98608758+samratsingh02@users.noreply.github.com>
2024-06-25 16:39:01 +0530

    Create style.js

---
+++ Freind_request_card/style.js
@@ -0,0 +1,57 @@
+html,body{
+          height: 100%;
+          width: 100%;
+
+
+}
+
+
+body{
+          background-color: cornflowerblue;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          
+
+}
+
+.card{
+          height: 400px;
+          width: 300px;
+          border-radius: 10px;
+          background-color: rgb(241, 231, 231);
+          padding: 20px;
+          text-align: center;
+
+}
+.img{
+
+width: 100%;
+height: 50%;
+object-fit:cover;
+object-position: center;
+border-radius: 10px;
+
+  
+
+}
+h5{
+          font-size: 25px;
+          color: red;
+          margin: 25px;
+         
+          
+}
+button{
+         
+          background-color: cadetblue;
+          width: 100px;
+          height: 30px;
+          color: white;   
+          font-size:12px;
+          font-weight: bold;
+         border-radius: 5px;
+         border:none;
+         cursor: pointer;
+
+}
